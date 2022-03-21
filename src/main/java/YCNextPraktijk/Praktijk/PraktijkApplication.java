04ce9ef94ba303e1abff3ae3cc376002c0d000c4
DutchAries <lahidi@outlook.com>
2022-03-21 09:38:04 +0100

    eerste .gitignore

---
+++ src/main/java/YCNextPraktijk/Praktijk/PraktijkApplication.java
@@ -0,0 +1,13 @@
+package YCNextPraktijk.Praktijk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraktijkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraktijkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/YCNextPraktijk/Praktijk/PraktijkApplicationTests.java
@@ -0,0 +1,13 @@
+package YCNextPraktijk.Praktijk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraktijkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
