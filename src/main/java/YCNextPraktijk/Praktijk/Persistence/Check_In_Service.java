094669977e04a948e0330819472085714b63a9da
Swidadi <bosmasjoerd@gmail.com>
2022-03-24 16:16:53 +0100

    toegevoegd: optie om via url checkins toe te voegen zoekfunctie per bordspel checkin via de body toevoegen

--- src/main/java/YCNextPraktijk/Praktijk/Persistence/Check_In_Service.java
+++ src/main/java/YCNextPraktijk/Praktijk/Persistence/Check_In_Service.java
@@ -11,8 +11,25 @@ public class Check_In_Service {
 	@Autowired
 	private Check_In_Repository cir;
 	
+	public Iterable<Check_In> zoekCheckin(String s){
+		return cir.findCheck_InsByBordspel(s);
+	}
+	
+	public Iterable<Check_In> geefCheckinsTerug() {
+		return cir.findAll();	
+	}
+	
+	public void allesTellen() {
+		long aantal = cir.count();
+		System.out.println(aantal);
+	}
+	
 	public void slaCheck_InOp() {
 		cir.save(new Check_In());
 	}
 
+	public void slaDezeCheck_InOp(Check_In ci) {
+		cir.save(ci);
+	}
+	
 }
--- src/main/java/YCNextPraktijk/Praktijk/Rest/Check_In_Endpoint.java
+++ src/main/java/YCNextPraktijk/Praktijk/Rest/Check_In_Endpoint.java
@@ -2,18 +2,54 @@ package YCNextPraktijk.Praktijk.Rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import YCNextPraktijk.Praktijk.Model.Check_In;
 import YCNextPraktijk.Praktijk.Persistence.Check_In_Service;
+
 
 @RestController
 public class Check_In_Endpoint {
 	@Autowired
 	private Check_In_Service cis;
 	
-	@GetMapping("/test")
-	public void testmethode() {
-		System.out.println("test");
-		cis.slaCheck_InOp();
+	
+	@GetMapping("/zoekcheckin/{query}")
+	public Iterable <Check_In> eenCheckin(@RequestParam String bordspel){
+		System.out.println("specific checkin");
+		return cis.zoekCheckin(bordspel);
+	}
+	
+	@GetMapping("/allecheckins")
+	public Iterable<Check_In> alleCheckins() {
+		System.out.println("alle checkins");
+		return cis.geefCheckinsTerug();
+	}
+	
+	@GetMapping("/aantalcheckins")
+	public void aantalCheckins() {
+		System.out.println("aantal checkins");
+		cis.allesTellen();
+	}
+	
+	@GetMapping("/checkin/{spel}/{locatie}/{rating}/{review}/{win}")
+	public void slaNieuweCheck_InOp(@PathVariable String spel, @PathVariable String locatie, @PathVariable float rating,
+			@PathVariable String review, @PathVariable boolean win) {
+		System.out.println("test: " + spel);
+		Check_In ci = new Check_In();
+		ci.setBordspel(spel); ci.setLocatie(locatie); 
+		ci.setRating(rating); ci.setReview(review); 
+		ci.setWin(win);
+		cis.slaDezeCheck_InOp(ci);
+	}
+	@PostMapping("/test2")
+	public void testPostMethode(@RequestBody Check_In checkin) {
+		System.out.println(checkin.getReview());
+		cis.slaDezeCheck_InOp(checkin);
+		// Hier moet je in Postman in de body van de request de variabelen zetten
 	}
 }
