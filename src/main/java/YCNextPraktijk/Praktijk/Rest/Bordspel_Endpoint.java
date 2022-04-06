b9fedabe163645ae97681b25507ee0f18211add0
David Nieuwenhuizen <david@laudav.nl>
2022-04-06 09:50:28 +0200

    Rating van bordspel berekenen en rating niet null constraint

--- src/main/java/YCNextPraktijk/Praktijk/Rest/Bordspel_Endpoint.java
+++ src/main/java/YCNextPraktijk/Praktijk/Rest/Bordspel_Endpoint.java
@@ -3,10 +3,13 @@ package YCNextPraktijk.Praktijk.Rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import YCNextPraktijk.Praktijk.Model.Bordspel;
+import YCNextPraktijk.Praktijk.Model.CheckIn;
 import YCNextPraktijk.Praktijk.Persistence.Bordspel_Service;
+import YCNextPraktijk.Praktijk.Persistence.Check_In_Service;
 
 @RestController
 @RequestMapping("api/bordspellen")
@@ -15,9 +18,23 @@ public class Bordspel_Endpoint {
 	@Autowired
 	private Bordspel_Service bs;
 	
+	@Autowired
+	private Check_In_Service cs;
+	
 	@GetMapping
 	public Iterable<Bordspel> alleBordspellen() {
 		System.out.println("alle checkins");
 		return bs.geefBordspellenTerug();
+	}
+	
+	@GetMapping("rating")
+	public float rating(@RequestParam String naam) {
+		Iterable<CheckIn> cl = cs.zoekCheckin(naam);
+		float total = 0, count = 0;
+		for (CheckIn c : cl) {
+			total += c.getRating();
+			count++;
+		}
+		return total/count;
 	}
 }
