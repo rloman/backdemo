b531f4ad9c6a313da473409bfb59e0e12155441a
Martijn <martijn.traas@live.nl>
2022-04-06 11:36:29 +0200

    Backend POST voor bordspel

--- src/main/java/YCNextPraktijk/Praktijk/Rest/Bordspel_Endpoint.java
+++ src/main/java/YCNextPraktijk/Praktijk/Rest/Bordspel_Endpoint.java
@@ -2,10 +2,13 @@ package YCNextPraktijk.Praktijk.Rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import YCNextPraktijk.Praktijk.Model.Bordspel;
+import YCNextPraktijk.Praktijk.Model.Gebruiker;
 import YCNextPraktijk.Praktijk.Persistence.Bordspel_Service;
 
 @RestController
@@ -13,11 +16,16 @@ import YCNextPraktijk.Praktijk.Persistence.Bordspel_Service;
 public class Bordspel_Endpoint {
 
 	@Autowired
-	private Bordspel_Service bs;
+	private Bordspel_Service bs; 
 	
 	@GetMapping
 	public Iterable<Bordspel> alleBordspellen() {
 		System.out.println("alle checkins");
 		return bs.geefBordspellenTerug();
+	}
+	
+	@PostMapping
+	public void bordspel(@RequestBody Bordspel bo) {
+		bs.slaBordspelOp(bo);	
 	}
 }
